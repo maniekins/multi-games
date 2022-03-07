d5f3ff7745553abb3c4a81715c323b30a62db197
Piotr Sawicki <piotr.sawicki@onespan.com>
2022-03-07 14:54:32 +0100

    eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,45 @@
 module.exports = {
-  parser: '@typescript-eslint/parser', // Specifies the ESLint parser
   extends: [
     'plugin:react/recommended', // Uses the recommended rules from @eslint-plugin-react
     'plugin:@typescript-eslint/recommended', // Uses the recommended rules from @typescript-eslint/eslint-plugin
-    'prettier', // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
-    'plugin:prettier/recommended', // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+    // 'prettier', // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
+    // 'plugin:prettier/recommended', // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
   ],
+  parser: '@typescript-eslint/parser', // Specifies the ESLint parser
   parserOptions: {
-    ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
-    sourceType: 'module', // Allows for the use of imports
+    createDefaultProgram: true,
     ecmaFeatures: {
       jsx: true, // Allows for the parsing of JSX
     },
+    ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
+    project: './tsconfig.json',
+    sourceType: 'module', // Allows for the use of imports
+    tsconfigRootDir: __dirname,
   },
   rules: {
     // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
     // e.g. "@typescript-eslint/explicit-function-return-type": "off",
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-explicit-any': ['error', { ignoreRestArgs: false }],
+    'consistent-return': 'off',
+    'import/no-extraneous-dependencies': 'off',
+    'import/no-unresolved': 'off',
+    'import/prefer-default-export': 'off',
+    'lines-between-class-members': ['error', 'always', { exceptAfterSingleLine: true }],
+    'no-param-reassign': 'off',
+    'react/jsx-filename-extension': 'off',
+    'react/jsx-props-no-spreading': 'off',
+    'react/prop-types': 'off',
+    'react-hooks/exhaustive-deps': 'off',
+    'sort-keys': [
+      'error',
+      'asc',
+      {
+        caseSensitive: true,
+        minKeys: 2,
+        natural: true,
+      },
+    ],
   },
   settings: {
     react: {
